b55a025e6d9367334358c66b2328694988931074
ianbjohn <ianbjohn@iu.edu>
2019-08-14 14:16:58 -0400

    Steps and inverse steps all work properly, but decrypting the encrypted message still results in gibberish. The documentation I read said to do the decryption steps in a different order, which doesn't make any sense to me. But I'll have to look into it more.

--- aes_test.c
+++ aes_test.c
@@ -3,44 +3,21 @@
 
 int main() {
   byte key[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
-  byte round_keys[11][16];
   byte plaintext[16] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10};
 
-  byte state[4][4];
+  aes_encrypt(plaintext, key);
 
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++)
-      state[j][i] = plaintext[(i * 4) + j];  //column-major traversal
-  }
+  FILE* f = fopen("ciphertext", "w");
+  for (int i = 0; i < 16; i++)
+    fputc(plaintext[i], f);
+  fclose(f);
 
-  //print real quick in row-major order
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++)
-      printf("%x ", state[i][j]);
-    printf("\n");
-  }
-  printf("\n");
+  aes_decrypt(plaintext, key);
 
-  //test shiftrows
-  aes_shiftrows(state);
-
-  //print result
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++)
-      printf("%x ", state[i][j]);
-    printf("\n");
-  }
-  printf("\n");
-
-  //test invshiftrows
-  aes_invshiftrows(state);
-
-  //print result (should be original state)
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++)
-      printf("%x ", state[i][j]);
-    printf("\n");
-  }
+  f = fopen("plaintext", "w");
+  for (int i = 0; i < 16; i++)
+    fputc(plaintext[i], f);
+  fclose(f);
 
   return 0;
 }
